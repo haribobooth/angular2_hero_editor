055de1a75b8d83887730323b3e89806ef674b3ad
Harrison Booth <harrisonbooth@ymail.com>
2017-04-25 12:20:30 +0100

    correct formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,19 @@ import HEROES from './HeroSeed'
 
   <h2>My Heroes</h2>
   <ul class="heroes">
-  <li *ngFor="let hero of heroes" (click)="onSelect(hero)">
-  <span class="badge">{{hero.id}}</span> {{hero.name}}
-  </li>
+    <li *ngFor="let hero of heroes" (click)="onSelect(hero)"
+      [class.selected]="hero === selectedHero">
+      <span class="badge">{{hero.id}}</span> {{hero.name}}
+    </li>
   </ul>
 
   <div *ngIf="selectedHero">
-  <h2>{{selectedHero.name}}</h2>
-  <div><label>id: </label>{{selectedHero.id}}</div>
-  <div>
-  <label>name: </label>
-  <input [(ngModel)]="selectedHero.name" placeholder="name">
-  </div>
+    <h2>{{selectedHero.name}}</h2>
+    <div><label>id: </label>{{selectedHero.id}}</div>
+    <div>
+      <label>name: </label>
+      <input [(ngModel)]="selectedHero.name" placeholder="name">
+    </div>
   </div>
   `,
   styles: [`
@@ -82,5 +83,5 @@ import HEROES from './HeroSeed'
     onSelect(hero: Hero): void {
       this.selectedHero = hero;
     };
-    
+
   }
