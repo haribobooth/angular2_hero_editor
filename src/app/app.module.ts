f38cdfebd0518968714f6f532fcdbf7695bcd701
Harrison Booth <harrisonbooth@ymail.com>
2017-04-25 13:44:06 +0100

    add application shell and move master component to its own file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HeroDetailComponent } from './hero-detail.component';
 
 import { AppComponent }  from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [ HeroService ],
+  bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
